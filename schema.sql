89a9a187642032f9a41c38a9f95b02000ac6fa9a
Obed N Munoz <obed.n.munoz@gmail.com>
2014-11-05 00:07:16 -0600

    Initial Commit
    Signed-off-by: Obed N Munoz <obed.n.munoz@gmail.com>

--- schema.sql
+++ schema.sql
@@ -1,9 +1,7 @@
-use flaskr;
 
 drop table if exists entries;
 create table entries (
-  id integer auto_increment,
+  id integer primary key autoincrement,
   title text not null,
   text text not null
-  primary key (id)
 );
